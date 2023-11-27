b994898ffe2126a173129cdbc60772623694684f
Julia Reva <98896958+juliareva@users.noreply.github.com>
2023-11-26 17:24:22 -0800

    link verification added

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { ReactComponent as Drag_and_Drop } from "../assets/images/icon-drag-and-drop.svg";
 
 const Link = ({idx, links, setLinks}) => {
+    const [alert, setAlert] = useState(""); 
+    const [touched, setTouched] = useState(false); 
+    const [platform, setPlatform] = useState(""); 
+
+const handleEmpty = (event) => {setAlert(event.target.value);}
+
   return (
     <div>
       <div className="link">
@@ -14,15 +20,20 @@ const Link = ({idx, links, setLinks}) => {
         <div className="link_body">
           <label for="platforms">Platform</label>
 
-          <select name="platforms" id="platforms">
+          <select name="platforms" id="platforms" value={platform} onChange={(e) => {
+              setPlatform(e.target.value);
+            }} >
             <option value="github">Github</option>
             <option value="youtube">Youtube</option>
             <option value="linkedin">LinkedIn</option>
             <option value="instagram">Instagram</option>
+            <option value="twitter">Twitter</option>
           </select>
 
           <label for="link">Link</label>
-          <input type="url" placeholder="Link placeholder here" required />
+          <input type="url" placeholder="Link placeholder here" required onChange={handleEmpty} onFocus={() => {setTouched(true)}}/>
+        {alert.length < 1 && touched ? <p>Can't be empty</p> : ""}
+        {alert.length > 0 && !alert.includes("github") ? <p>Please check the URL</p> : ""}
         </div>
       </div>
     </div>
